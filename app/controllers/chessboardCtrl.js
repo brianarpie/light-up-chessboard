f324db90699c2c6cbdd39bebb44790f61a91ae63
Brian Arpie <brian@crisply.com>
2014-09-15 23:00:27 -0400

    add purple for equally contested squares

--- app/controllers/chessboardCtrl.js
+++ app/controllers/chessboardCtrl.js
@@ -19,60 +19,6 @@ app.controller('ChessboardController', ['$rootScope','$scope',
 
     }
 
-    // for now i can put these methods here and then later on re-organize them
-    // we dont want everything going in the chessboard controller
-    // on the things that directly influence/touch the chessbaord itself.
-
-    // function drag(ev) {
-    //   // debugger;
-    //   ev.dataTransfer.setData("text/html", ev.target.id);
-    // }    
-
-    // function precompute(ev) {
-    //    ev.preventDefault();
-    // }
-
-    // function drop(ev) {
-    //   ev.preventDefault();
-    //   var data = ev.dataTransfer.getData("imgUrl");
-    //   ev.target.appendChild(document.getElementById(data));
-
-    //   console.log('dropping event finished firing!');
-    // }
-
-    // $scope.dragData;
-
-    // data is the clone of the piece object for a given square
-    $scope.onDropComplete = function(event, data, x, y) {
-
-      // if (event.isPawn || searchArray(event.legalMoves(), [x,y])) {
-        // $scope.squares[x][y].addPiece(event);
-
-      if ($scope.squares[$scope.current_x][$scope.current_y].piece.color) {
-        if (event.color === 'white') {
-          $scope.removeWhitePiece(event, $scope.current_x, $scope.current_y);
-
-        } else {
-          $scope.removeBlackPiece(event, $scope.current_x, $scope.current_y);
-        }
-        $scope.squares[$scope.current_x][$scope.current_y].piece = {};
-        document.getElementById($scope.current_x.toString().concat($scope.current_y)).src = ''
-      }
-        $scope.squares[x][y].piece = event;
-        $scope.squares[x][y].piece.x = x;
-        $scope.squares[x][y].piece.y = y;
-        if (event.color == 'white') {
-
-          $scope.addWhitePiece(event, x, y);
-        } else {
-          $scope.addBlackPiece(event, x, y);
-        }
-
-      // }
-// debugger;
-    }
-
-
     function searchArray(arr, val) {
       var hash = {};
       for (var i = 0, len = arr.length; i < len; i++) {
@@ -86,91 +32,71 @@ app.controller('ChessboardController', ['$rootScope','$scope',
 
     }
 
-    $scope.precompute = function(event, data) {
-      // get all legal moves!
-                    $scope.current_x = event.x;
-      $scope.current_y = event.y;
-      // $scope.dragData = data;
-      // console.log(event.x);
-      // console.log(event.y);
-      // these x and y need to be cleared!
-      // console.log(data);
-      // $scope.squares[event.x][event.y].piece.
+    $scope.onDropComplete = function(event, data, x, y) {
+      var piece = event;
+      var curr_x = $scope.current_x;
+      var curr_y = $scope.current_y;
+      if ($scope.squares[curr_x][curr_y].piece.color) {
+        if (piece.color === 'white') {
+          $scope.removeWhitePiece(piece, curr_x, curr_y);
+        } else {
+          $scope.removeBlackPiece(piece, curr_x, curr_y);
+        }
+        $scope.squares[curr_x][curr_y].piece = {};
+      }
 
-      // $scope.squares[event.x][event.y].piece.imgUrl('');
-      // debugger;
-      // $scope.current_x = event.x;
-      // $scope.current_y = event.y;
-      // if ($scope.squares[$scope.current_x][$scope.current_y].piece.color) {
-      //   if (event.color === 'white') {
-      //     $scope.removeWhitePiece(event, $scope.current_x, $scope.current_y);
-
-      //   } else {
-      //     $scope.removeBlackPiece(event, $scope.current_x, $scope.current_y);
-      //   }
-      //   $scope.squares[$scope.current_x][$scope.current_y].piece = {};
-      //   document.getElementById($scope.current_x.toString().concat($scope.current_y)).src = ''
-      // }
-      // console.log(event);
-      // console.log(document.getElementById($scope.current_x.toString().concat($scope.current_y)));
-        
-        
-      // console.log('finished removing piece from square');
-      // console.log($scope.current_x , $scope.current_y)
-      // debugger;
-
-
-      // console.log('precomputing...')
-
+      $scope.squares[x][y].piece = piece;
+      $scope.squares[x][y].piece.x = x;
+      $scope.squares[x][y].piece.y = y;
+      if (piece.color == 'white') {
+        $scope.addWhitePiece(piece, x, y);
+      } else {
+        $scope.addBlackPiece(piece, x, y);
+      }
     }
 
-
-
-
+    // future use
+    $scope.precompute = function(event, data) {
+      $scope.current_x = event.x;
+      $scope.current_y = event.y;
+    }
 
     $scope.getBackgroundColor = function(x, y) {
       var color, totalCounters;
-      // if ($scope.squares[x][y].)
-      totalCounters = $scope.squares[x][y].white_counters - $scope.squares[x][y].black_counters
+      var black = $scope.squares[x][y].black_counters;
+      var white = $scope.squares[x][y].white_counters;
+      totalCounters = white - black;
+      if (white + black === 0 ) {
+        return 'white';
+      }
       color = $scope.opacity_chart(totalCounters);
+      if (!color) debugger;
       return 'rgba('+color.r+','+color.g+','+color.b+','+color.a+')';
     }
 
     $scope.getImage = function(x, y) {
-      // if ($scope.squares[x][y].piece) debugger;
-      // debugger;
-      // if (x ==7 && y=== 7) {
-      //   // debugger;
-      //   return '';
-      //   console.log('getting Image for h1!');
-      // }
       if ($scope.squares[x][y].piece && $scope.squares[x][y].piece.color) {
-        // debugger;
         return $scope.squares[x][y].piece.imgUrl($scope.squares[x][y].piece.color);
       } else {
-        return '';
+        return ' ';
       }
-
-      // return ($scope.squares[x][y].piece && $scope.squares[x][y].piece.color) ? $scope.squares[x][y].piece.imgUrl($scope.squares[x][y].piece.color) : '';
     }
 
-    // $scope.getLegalMoves = function(x, y) {
-    //   return $scope.squares[x][y].piece.legalMoves();
-    // }
+    $scope.$watch('squareColor', function(id) {
+      console.log(id);
+    });
 
     $scope.addPiece = function(name, color, x, y) {
       $scope.squares[x][y].piece = getPieceObject(name);
       $scope.squares[x][y].piece.setPosition(x, y);
       
       $scope.squares[x][y].piece.color = color;
-      // if (x ==7 && y==7) {
-      //   debugger;
-      // }
       if (color == 'white') {
         $scope.addWhitePiece($scope.squares[x][y].piece, x, y);
       } else {
         $scope.addBlackPiece($scope.squares[x][y].piece, x, y);
       }
+
     }
 
     $scope.opacity_chart = function(heatLevel) {
@@ -188,7 +114,7 @@ app.controller('ChessboardController', ['$rootScope','$scope',
         case -1:
           return {r: 255, g: 0, b: 0, a: 0.1};
         case 0:
-          return {r: 0, g: 0, b: 0, a: 0};
+          return {r: 255, g: 0, b: 255, a: 0.15};
         case 1:
           return {r: 0, g: 0, b: 255, a: 0.1};
         case 2:
@@ -202,40 +128,7 @@ app.controller('ChessboardController', ['$rootScope','$scope',
         case 6:
           return {r: 0, g: 0, b: 255, a: 0.6};
         default:
-          return {r: 255, g: 255, b: 255};
-      }
-    }
-
-    $scope.color_chart = function(heat_level) {
-      switch(heat_level) {
-        case -6:
-          return {r: 155, g: 0, b: 0};
-        case -5:
-          return {r: 145, g: 0, b: 0};
-        case -4:
-          return {r: 135, g: 0, b: 0};
-        case -3:
-          return {r: 125, g: 0, b: 0};
-        case -2:
-          return {r: 115, g: 0, b: 0};
-        case -1:
-          return {r: 105, g: 0, b: 0};
-        case 0:
-          return {r: 0, g: 255, b: 0};
-        case 1:
-          return {r: 0, g: 0, b: 125};
-        case 2:
-          return {r: 0, g: 0, b: 135};
-        case 3:
-          return {r: 0, g: 0, b: 145};
-        case 4:
-          return {r: 0, g: 0, b: 155};
-        case 5:
-          return {r: 0, g: 0, b: 165};
-        case 6:
-          return {r: 0, g: 0, b: 175};
-        default:
-          return {r: 255, g: 255, b: 255};
+          return {r: 255, g: 255, b: 255, a:0};
       }
     }
 
@@ -248,53 +141,12 @@ app.controller('ChessboardController', ['$rootScope','$scope',
     };
 
     $scope.addBlackCounter = function(x, y) {
-      // debugger;
       $scope.squares[x][y].black_counters++;
     };
 
     $scope.deleteBlackCounter = function(x, y) {
       $scope.squares[x][y].black_counters--;
     };
-
-    // $scope.printBlackCounters = function() {
-    //   var x, y, max, buf = [];
-    //   for (y = 0, max = 8; y < max; y++) {
-    //     for (x = 0; x < max; x++) {
-    //       buf.push($scope.squares[x][y].black_counters);
-    //     }
-    //     console.log(buf);
-    //     buf = [];
-    //   }
-    // }
-    // $scope.printWhiteCounters = function() {
-    //   var x, y, max, buf = [];
-    //   for (y = 0, max = 8; y < max; y++) {
-    //     for (x = 0; x < max; x++) {
-    //       buf.push($scope.squares[x][y].white_counters);
-    //     }
-    //     console.log(buf);
-    //     buf = [];
-    //   }
-    // }
-
-    // $scope.print = function() {
-    //   var x, y, max, diff, buf = [], color = {};
-    //   for (y = 0, max = 8; y < max; y++) {
-    //     for (x = 0; x < max; x++) {
-    //       diff = ($scope.squares[x][y].white_counters - $scope.squares[x][y].black_counters);
-    //       // useful but not yet implm.
-    //       // color = $scope.color_chart(diff);
-    //       // set_background(x, y, color);
-    //       if (diff >= 0) {
-    //         diff = "+" + diff;
-    //       }
-    //         buf.push('['+diff+']');
-    //     }
-    //     console.log('\n');
-    //     console.log(buf);
-    //     buf = [];
-    //   }
-    // }
 
     $scope.clear = function() {
       var x, y, max;

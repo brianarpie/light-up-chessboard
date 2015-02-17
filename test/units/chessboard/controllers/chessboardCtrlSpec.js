caf124bec7502935a410ec2795e9418cb6ef87dc
Brian Arpie <brian@crisply.com>
2015-02-17 02:03:20 -0500

    first test written

--- test/units/chessboard/controllers/chessboardCtrlSpec.js
+++ test/units/chessboard/controllers/chessboardCtrlSpec.js
@@ -2,7 +2,12 @@
 
   "use strict";
 
+  describe("Chessboard Controller Spec", function() {
 
-  
+    it("should assert true to be true", function() {
+      expect(true).toBe(true);
+    });
+
+  });
 
 })();

74080d282efaab08e67dec01c300aee301e4582b
Brian Arpie <brian.arpie@gmail.com>
2015-10-17 16:42:14 -0400

    fill in gaps from old copy of master

---
+++ test/units/chessboard/controllers/chessboardCtrlSpec.js
@@ -0,0 +1,28 @@
+(function() {
+
+  "use strict";
+
+  describe("Chessboard Controller Spec", function() {
+    var $scope, $rootScope, $controller;
+
+    beforeEach(module('LaLuzDeAjedrez'));
+    beforeEach(inject(function($injector) {
+      $rootScope = $injector.get('$rootScope');
+      $scope = $rootScope.$new();
+      $controller = $injector.get('$controller');
+
+      $controller("ChessboardController", {
+        "$scope": $scope
+      });
+    }));
+
+    it('should set the board square "selected" attr to true when clicked', function() {
+      var x = 4, y = 5;
+      expect($scope.board[x][y].selected).toBe(undefined);
+      $scope.clickPiece(x, y);
+      expect($scope.board[x][y].selected).toBe(true);
+    });
+
+  });
+
+})();

8dbe7c0fea599174f6dab7f44039fbebdbbec1db
Brian Arpie <brian.arpie@gmail.com>
2015-10-19 11:28:19 -0400

    update dependencies injected to karma runner

--- karma.conf.js
+++ karma.conf.js
@@ -24,7 +24,7 @@ module.exports = function(config) {
       'node_modules/angular/angular.js',
       'node_modules/angular-route/angular-route.js',
       'node_modules/angular-mocks/angular-mocks.js',
-      'bower_components/**/*.js',
+      'bower_components/ng-draggable/ngDraggable.js',
       'app/app.js',
       'app/**/*.js',
 

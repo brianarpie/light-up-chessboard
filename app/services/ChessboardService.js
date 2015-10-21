28182270c44ac4b2276b3e0d3ebb13cc082f9f55
Brian Arpie <brian.arpie@gmail.com>
2015-10-20 23:18:34 -0400

    new proposed directory structure

--- app/services/ChessboardService.js
+++ app/services/ChessboardService.js
@@ -4,10 +4,35 @@
 
   var app = angular.module("LaLuzDeAjedrez");
 
-  app.service("ChessboardService", [function() {
-    var service = this;
+  // TODO: extrapolate this to a CONSTANT
+  var ASCII_INDEX = 97;
 
-    service.secret_key ="a2";
+  app.service("ChessboardService", ["PawnService", function(PawnService) {
+    var chessboard = this;
+
+    chessboard.lookupSquare = function(letter, number) {
+      return chessboard.board[letter.charCodeAt(0)-ASCII_INDEX][number-1];
+    };
+
+    chessboard.setupPieces = function() {
+
+    };
+
+    chessboard.init = function() {
+      chessboard.board = new Array(8);
+      for(var i = 0, len = 8; i < len; i++) {
+        chessboard.board[i] = new Array(8);
+      }
+      var x, y, len = 8;
+      for (x = 0 ; x < len; x++) {
+        for (y = 0 ; y < len; y++) {
+          chessboard.board[x][y] = {};
+          chessboard.board[x][y].white_counters = 0;
+          chessboard.board[x][y].black_counters = 0;
+        }
+      }
+
+    }
 
   }]);
 
--- test/unit/services/ChessboardService.spec.js
+++ test/unit/services/ChessboardService.spec.js
@@ -3,11 +3,20 @@
   "use strict";
 
   describe("Chessboard Service Unit Tests", function() {
-    var ChessboardService;
+    var ChessboardService, PawnService;
+
     beforeEach(module("LaLuzDeAjedrez"));
-    beforeEach(inject(function(_ChessboardService_) {
+    beforeEach(inject(function(_ChessboardService_, _PawnService_) {
       ChessboardService = _ChessboardService_;
+      PawnService = _PawnService_;
     }));
+
+    it('should return a letter/number board reference', function() {
+      ChessboardService.init();
+      var piece = ChessboardService.lookupSquare("a", 2);
+      console.log(piece);
+      expect(piece).toBe("knight");
+    });
 
   });
 

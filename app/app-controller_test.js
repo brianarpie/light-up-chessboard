602d8472c025d4486a0a1ef23f95ea461127d10c
Brian Arpie <brian.arpie@gmail.com>
2015-10-21 00:02:39 -0400

    new structure complete

---
+++ app/app-controller_test.js
@@ -0,0 +1,10 @@
+(function() {
+
+  "use strict";
+
+  it('passes CI with flying colors', function() {
+    var everythingIsAwesome = true;
+    expect(everythingIsAwesome).toBe(true);
+  });
+
+})();

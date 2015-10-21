8f49c7f48459b5b8b7e2698ecec59c3a596ba44b
Brian Arpie <brian.arpie@gmail.com>
2015-10-21 19:17:57 -0400

    add build capability to grunt config

--- config/grunt.conf.js
+++ config/grunt.conf.js
@@ -65,7 +65,9 @@
         options: {
           args: {
             sauceUser: process.env.SAUCE_USERNAME,
-            sauceKey: process.env.SAUCE_ACCESS_KEY
+            sauceKey: process.env.SAUCE_ACCESS_KEY,
+            "tunnel-identifier": process.env.TRAVIS_JOB_NUMBER,
+            build: process.env.TRAVIS_BUILD_NUMBER
           },
         }
       }

a326bb36229e519ac89d146dbee023883a7970c0
Brian Arpie <brian.arpie@gmail.com>
2015-10-23 00:45:23 -0400

    chessboard service can add a piece and has a algebraic notation converter

--- app/chessboard/chessboard-service.js
+++ app/chessboard/chessboard-service.js
@@ -18,12 +18,29 @@
       });
     }
 
+    function addPiece(piece, color, location) {
+      var locationIndex = convertAlgebraicNotation(location);
+      var targetSquare = chessboard[locationIndex[0]][locationIndex[1]];
+      targetSquare.piece = piece;
+      targetSquare.color = color;
+    }
+
+    function convertAlgebraicNotation(notation) {
+      var correctionFactor = "a".charCodeAt(0);
+      var firstIndex = Math.abs(notation.charAt(1) - 8); // correction factor for row indexing
+      var secondIndex = notation.charAt(0).toLowerCase().charCodeAt(0) - correctionFactor;
+
+      return [firstIndex, secondIndex];
+    }
+
     init();
 
     return {
       getBoard: function() {
         return chessboard;
-      }
+      },
+      addPiece: addPiece,
+      convertAlgebraicNotation: convertAlgebraicNotation
     };
   }]);
 
--- app/chessboard/chessboard-service_test_.js
+++ app/chessboard/chessboard-service_test_.js
@@ -17,11 +17,38 @@
 
     it("should create the chessboard as an 8 x 8 array", function() {
       var chessboard = ChessboardService.getBoard();
-      var countOfItems = _.reduce(chessboard, function(memo, array) {
-        return memo + array.length;
-      }, 0);
+
       expect(chessboard.length).toBe(8);
-      expect(countOfItems).toBe(64);
+      expect(chessboard).toEqual(jasmine.any(Array));
+
+      _.each(chessboard, function(row) {
+        expect(row).toEqual(jasmine.any(Array));
+        expect(row.length).toEqual(8);
+      });
+    });
+
+    it("should provide a function to add a piece to a square", function() {
+      var chessboard,
+          locationIndex,
+          targetSquare,
+          piece = "pawn",
+          color = "white",
+          square = "a1";
+
+      ChessboardService.addPiece(piece, color, square);
+      chessboard = ChessboardService.getBoard();
+      locationIndex = ChessboardService.convertAlgebraicNotation(square);
+      targetSquare = chessboard[locationIndex[0]][locationIndex[1]];
+      expect(targetSquare.piece).toBe("pawn");
+      expect(targetSquare.color).toBe("white");
+    });
+
+    it("should map algebraic notation to an index on the chessboard", function() {
+      // test top-left and bottom-right corner squares
+      var a8 = ChessboardService.convertAlgebraicNotation("a8");
+      expect(a8).toEqual([0,0]);
+      var h1 = ChessboardService.convertAlgebraicNotation("h1");
+      expect(h1).toEqual([7,7]);
     });
 
   });

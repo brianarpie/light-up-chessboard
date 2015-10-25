2762a873d0af9dd4314783b5dc3f5d61407f0dd8
Brian Arpie <brian.arpie@gmail.com>
2015-10-25 01:52:14 -0400

    ChessboardCtrl will setup starting pieces. rooks in place with tests

--- app/chessboard/chessboard-controller.js
+++ app/chessboard/chessboard-controller.js
@@ -4,8 +4,13 @@
 
   var app = angular.module("chessboard");
 
-  app.controller("ChessboardCtrl", function() {
-
-  });
+  app.controller("ChessboardCtrl", [
+    "ChessboardService",
+    function(ChessboardService) {
+    ChessboardService.addPiece("rook", "white", "a1");
+    ChessboardService.addPiece("rook", "white", "h1");
+    ChessboardService.addPiece("rook", "black", "a8");
+    ChessboardService.addPiece("rook", "black", "h8");
+  }]);
 
 })();
--- app/chessboard/chessboard-controller_test.js
+++ app/chessboard/chessboard-controller_test.js
@@ -3,15 +3,40 @@
   "use strict";
 
   describe("chessboard-controller unit tests", function() {
-    var controller, scope;
+    var controller, scope, ChessboardService;
 
     beforeEach(module("chessboard"));
-    beforeEach(inject(function($rootScope, $controller) {
+    beforeEach(inject(function($rootScope, $controller, _ChessboardService_) {
       scope = $rootScope.$new();
       controller = $controller("ChessboardCtrl", {
         $scope: scope
       });
+      ChessboardService = _ChessboardService_;
     }));
+
+    it("should setup rooks on a8, h8 (black) and a1, h1 (white)", function() {
+      var blackRookSquares = [];
+      blackRookSquares.push(ChessboardService.getSquare("a8"));
+      blackRookSquares.push(ChessboardService.getSquare("h8"));
+
+      _.each(blackRookSquares, function(square) {
+        expect(square.piece).toBe("rook");
+        expect(square.color).toBe("black");
+      });
+
+      var whiteRookSquares = [];
+      whiteRookSquares.push(ChessboardService.getSquare("a1"));
+      whiteRookSquares.push(ChessboardService.getSquare("h1"));
+
+      _.each(whiteRookSquares, function(square) {
+        expect(square.piece).toBe("rook");
+        expect(square.color).toBe("white");
+      });
+    });
+
+    it("should setup the king on e1", function() {
+      // ChessboardService.getSquare("e1")
+    });
 
   });
 
